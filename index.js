d80de36354b70e12dfcc807bfba6276ea856001f
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-06-12 12:39:21 -0400

    added js

--- index.js
+++ index.js
@@ -19,4 +19,28 @@ let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
 
+let dogDes = document.createElement ('h3')
+dogDes.textContent = 'Description:'
+dogDetails.append (dogDes)
 
+let dogDes2 = document.createElement ('p')
+dogDes2.textContent = 'This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.'
+dogDetails.append (dogDes2)
+
+let dogDes3 = document.createElement ('h3')
+dogDes3.textContent = 'Feeding Times:'
+dogDetails.append (dogDes3)
+
+let dogDes4 = document.createElement ('ul')
+
+let time1 = document.createElement ('li')
+let time2 = document.createElement ('li')
+let time3 = document.createElement ('li')
+
+time1.textContent ='9:00 am'
+time2.textContent = '12:00 pm'
+time3.textContent = '5:00 pm'
+
+dogDes4.append (time1, time2, time3)
+
+dogDetails.append (dogDes4)
